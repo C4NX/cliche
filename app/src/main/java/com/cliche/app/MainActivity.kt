8b882387c70f07ef5c1aff328a7b0ad39e18dd82
ElStefanoNacho <134319084+elstefanonacho@users.noreply.github.com>
2026-01-24 17:05:58 +0100

    PARTIAL: Main custom toolbar
    # Conflicts:
    #	app/src/main/java/com/cliche/app/MainActivity.kt
    #	app/src/main/res/layout/activity_login.xml
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/cliche/app/MainActivity.kt
+++ app/src/main/java/com/cliche/app/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.cliche.app
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -12,6 +15,7 @@ import com.cliche.app.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,12 +23,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.topAppBar)
+
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-//         Passing each menu ID as a set of Ids because each
-//         menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
@@ -32,6 +39,37 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        supportActionBar?.hide()
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            supportActionBar?.title = getString(R.string.app_name)
+            if (appBarConfiguration.topLevelDestinations.contains(destination.id)) {
+                supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_account_circle)
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.top_app_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.settings -> {
+                // Code pour le clic sur Settings
+                Toast.makeText(this, "Settings cliqué !", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        return if (androidx.navigation.ui.NavigationUI.navigateUp(navController, appBarConfiguration)) {
+            true
+        } else {
+            Toast.makeText(this, "Profil cliqué !", Toast.LENGTH_SHORT).show()
+            true
+        }
     }
 }
